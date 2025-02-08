581b4ced0d740dc20ba07805c92b37434e6db471
Orlando <orlando.arias.j@gmail.com>
2025-02-08 18:54:22 -0300

    ola

--- src/components/1.5_Header.jsx
+++ src/components/1.5_Header.jsx
@@ -1,21 +1,15 @@
 export default function Navbar() {
   return (
-    <div id="Inicio" class="relative h-170 text-white overflow-hidden">
+    <div id="Inicio" class="relative h-190 text-white overflow-hidden">
       <div class="absolute inset-0">
-        <img
-          src="https://www.cined.com/content/uploads/2021/06/AdobeM1Mac_Featured-1300x750.jpg"
-          alt="Background Image"
-          class="object-cover object-center w-full h-full "
+      <video
+          src="https://res.cloudinary.com/dnd9wg0pi/video/upload/v1739049348/Motion_Graphic_3_uxpj2v.mp4"
+          className="object-cover object-center w-full h-full"
+          autoPlay
+          muted
+          loop
         />
-        <div class="absolute inset-0 h-full bg-gradient-to-r from-slate-900 to-slate-700 opacity-60"></div>
-      </div>
-
-      <div class="relative z-10 flex flex-col justify-center items-center h-full text-center">
-        <h1 class="mb-2 text-3xl lg:text-5xl text-gray-50 tracking-tight font-extrabold">
-          ZASION MOTION
-        </h1>
-        <p class="text-lg lg:text-xl text-gray-300 mb-8">EDITOR DE VIDEOS</p>
-      </div>
+    </div>
     </div>
   );
 }
