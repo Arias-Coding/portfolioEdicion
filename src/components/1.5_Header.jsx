a7b0af07a87a81a8782fea84229f66fd46b163be
Orlando <orlando.arias.j@gmail.com>
2025-03-01 19:20:52 -0300

    ola

--- src/components/1.5_Header.jsx
+++ src/components/1.5_Header.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <div id="Inicio" class="relative h-80 sm:h-100 md:h-190 text-white overflow-hidden">
       <div class="absolute inset-0">
       <video
-          src="https://res.cloudinary.com/dnd9wg0pi/video/upload/v1739049348/Motion_Graphic_3_uxpj2v.mp4"
+          src="https://res.cloudinary.com/dnd9wg0pi/video/upload/v1740867602/Output_2_z81jnn.mp4"
           className="object-cover object-center w-full h-full"
           autoPlay
           muted
