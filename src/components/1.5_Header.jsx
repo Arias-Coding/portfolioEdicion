afeb8cda60e62adfe1258b0f376d2c34d39ea916
Orlando <orlando.arias.j@gmail.com>
2025-05-29 18:29:50 -0300

    ola

--- src/components/1.5_Header.jsx
+++ src/components/1.5_Header.jsx
@@ -1,15 +1,18 @@
 export default function Navbar() {
   return (
-    <div id="Inicio" class="relative h-80 sm:h-100 md:h-190 text-white overflow-hidden">
+    <div
+      id="Inicio"
+      class="relative h-80 sm:h-100 md:h-190 text-white overflow-hidden md:min-h-[65vh]"
+    >
       <div class="absolute inset-0">
-      <video
+        <video
           src="https://res.cloudinary.com/dnd9wg0pi/video/upload/v1740867602/Output_2_z81jnn.mp4"
           className="object-cover object-center w-full h-full"
           autoPlay
           muted
           loop
         />
-    </div>
+      </div>
     </div>
   );
 }
